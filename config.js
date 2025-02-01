273359cf670d14436f166b265fcd5857d4b50277
Kyle6982 <wow4710@gmail.com>
2025-02-01 14:48:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -20,34 +20,6 @@ module.exports = {
       host: "lavav4.prplmoe.me",
       port: 1118,
       secure: false
-    },
-    {
-      name: "ChalresNaig Node",
-      password: "NAIGLAVA-dash.techbyte.host",
-      host: "lavahatry4.techbyte.host",
-      port: 3000,
-      secure: false
-    },
-     {
-      name: "INZEWORLD.COM (DE)",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
-    },
-    {
-      name: "Catfein ID",
-      password: "catfein",
-      host: "lava.catfein.com",
-      port: 4000,
-      secure: false
-    },
-    { 
-      name: "saher",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
     }
   ]
 }
